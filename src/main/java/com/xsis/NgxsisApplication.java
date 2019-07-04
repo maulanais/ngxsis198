c055e12c21a1a99c7451d36612f95caf9eca593e
PiPio <maulanacoconut@gmail.com>
2019-07-04 16:46:58 +0700

    upload awal

---
+++ src/main/java/com/xsis/NgxsisApplication.java
@@ -0,0 +1,13 @@
+package com.xsis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgxsisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgxsisApplication.class, args);
+	}
+
+}
